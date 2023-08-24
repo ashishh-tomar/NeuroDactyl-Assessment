9ca35ac64555da45cb776a87df5c2c36892a21b7
ashishh-tomar <ashishtomar692@gmail.com>
2023-08-25 02:19:37 +0530

    Final commit

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,24 +1,23 @@
-import footerStyles from '@/app/styles/footer.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
-
+import footerStyles from "@/app/styles/footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div id={footerStyles.page_container}>
-    
-    <footer className={footerStyles.footer}>
-     <Link href="/">
-                <Image src="/logo.svg" alt="logo" width={100} height={40}/>
-                <h3>NEURODACTYL</h3>
-            </Link>
-      <p>All Right Reserved, 2023. Neurodactyl, LLC.
-Address: Georgia, Tbilisi, Nadzaladevi District, Kursebi Street, N15. Identification Number: 400354488
-E-mail us: info@neurodactyl.com</p>
-    </footer>
+      <footer >
+        <Link href="/" className={footerStyles.content}>
+          <Image src="/logo.svg" alt="logo" width={100} height={40} />
+          <h3>NEURODACTYL</h3>
+        </Link>
+        <p>
+          All Right Reserved, 2023. Neurodactyl, LLC. Address: Georgia, Tbilisi,
+          Nadzaladevi District, Kursebi Street, N15. Identification Number:
+          400354488 E-mail us: info@neurodactyl.com
+        </p>
+      </footer>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
